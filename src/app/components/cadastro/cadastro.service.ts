7f537d6fd6669ced20059dfd42a00c1d1d02f0e1
Prisiciliane Valeska <prisciliane_valeska@hotmail.com>
2021-11-24 22:52:58 -0300

    comit Uc10

---
+++ src/app/components/cadastro/cadastro.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+import { Cadastro } from './cadastro.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CadastroService {
+
+  baseurl = "http://localhost:3001/cadastro"
+
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) { 
+
+  }
+  showMessege(msg: string): void{
+    this.snackBar.open(msg, 'X',{
+      duration: 6000,
+      verticalPosition: "bottom"
+    });
+  }
+  create(cadastro: Cadastro): Observable<Cadastro>{
+    return this.http.post<Cadastro>(this.baseurl, cadastro)
+  }
+  read(): Observable<Cadastro[]>{
+    return this.http.get<Cadastro[]>(this.baseurl)
+  }
+  readById(id: number): Observable<Cadastro> {
+    const url = `${this.baseurl}/${id}`
+    return this.http.get<Cadastro>(url)
+  }
+  updateCadastro(cadastro: Cadastro): Observable<Cadastro>{
+    const url = `${this.baseurl}/${cadastro.id}`
+    return this.http.put<Cadastro>(url, cadastro)
+  }
+  
+}
